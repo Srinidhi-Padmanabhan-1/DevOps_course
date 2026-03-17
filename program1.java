9446b6dba10027328ce7aebe08696133741f3ce7
Srinidhi-Padmanabhan-1 <srinidhiatsies@gmail.com>
2026-03-17 17:52:31 +0530

    Update program1.java

--- program1.java
+++ program1.java
@@ -2,5 +2,6 @@ public class program1{
     public static void main (String[] args){
         System.out.println("Jenkins Q4");
         System.out.println("POLL SCM Test");
+        System.out.println("Webhook Test");
     }
 }
